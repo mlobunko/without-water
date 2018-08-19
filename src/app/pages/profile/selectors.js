f0b5dea91e37fe6c3e88f71a4f197f792b20bdca
Mykhailo Lobunko <mykhailo.lobunko@hotmail.com>
2018-08-19 11:37:29 +0300

    changed imports

--- src/app/pages/profile/selectors.js
+++ src/app/pages/profile/selectors.js
@@ -2,17 +2,16 @@ import { createSelector } from 'reselect';
 
 import { NAME } from './constants';
 import * as model from './model';
-import city from './components/city';
-import languages from './components/languages';
-import social from './components/social';
+import components from './components';
 
 export const getAll = state => state[NAME];
 
-export const getCity = state => state[NAME][city.constants.NAME];
+export const getCity = state => state[NAME][components.city.constants.NAME];
 
-export const getLanguages = state => state[NAME][languages.constants.NAME];
+export const getLanguages = state =>
+  state[NAME][components.languages.constants.NAME];
 
-export const getSocial = state => state[NAME][social.constants.NAME];
+export const getSocial = state => state[NAME][components.social.constants.NAME];
 
 export const getSocialWebFirst = createSelector([getSocial], social =>
   model.placeWebIconFirst(social)
