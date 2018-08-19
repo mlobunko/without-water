2e2862f8ba0ae034dead6039271e45c796f185a7
Mykhailo Lobunko <mykhailo.lobunko@hotmail.com>
2018-08-19 17:41:33 +0300

    changed imports

--- src/app/pages/profile/tests/selectors.test.js
+++ src/app/pages/profile/tests/selectors.test.js
@@ -1,7 +1,8 @@
 import { NAME } from '../constants';
-import city from '../components/city';
-import languages from '../components/languages';
-import social from '../components/social';
+import components from '../components';
+// import city from "../components/city";
+// import languages from "../components/languages";
+// import social from "../components/social";
 
 import {
   getAll,
@@ -13,9 +14,9 @@ import {
 
 const initialState = {
   [NAME]: {
-    [city.constants.NAME]: 'London',
-    [languages.constants.NAME]: ['english'],
-    [social.constants.NAME]: [
+    [components.city.constants.NAME]: 'London',
+    [components.languages.constants.NAME]: ['english'],
+    [components.social.constants.NAME]: [
       { label: 'fb', link: '123' },
       { label: 'web', link: '321' }
     ]
@@ -36,7 +37,7 @@ describe('getCity', () => {
     const state = {
       ...initialState
     };
-    expect(getCity(state)).toEqual(state[NAME][city.constants.NAME]);
+    expect(getCity(state)).toEqual(state[NAME][components.city.constants.NAME]);
   });
 });
 
@@ -45,7 +46,9 @@ describe('getLanguages', () => {
     const state = {
       ...initialState
     };
-    expect(getLanguages(state)).toEqual(state[NAME][languages.constants.NAME]);
+    expect(getLanguages(state)).toEqual(
+      state[NAME][components.languages.constants.NAME]
+    );
   });
 });
 
@@ -54,7 +57,9 @@ describe('getSocial', () => {
     const state = {
       ...initialState
     };
-    expect(getSocial(state)).toEqual(state[NAME][social.constants.NAME]);
+    expect(getSocial(state)).toEqual(
+      state[NAME][components.social.constants.NAME]
+    );
   });
 });
 
