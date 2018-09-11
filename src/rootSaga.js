61fa81c9318978ef97fc2f347efe006fc4a10be1
Mykhailo Lobunko <mykhailo.lobunko@hotmail.com>
2018-09-11 17:51:39 +0300

    change rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,12 +1,28 @@
-import { all, fork } from 'redux-saga/effects';
+import { all, call, spawn } from 'redux-saga/effects';
+
 import logIn from 'app/pages/logIn';
 import news from 'app/pages/news';
 import profile from 'app/pages/profile';
 
 export default function* rootSaga() {
-  yield all([
-    fork(logIn.saga.submitLogIn),
-    fork(news.saga.fetchNews),
-    fork(profile.saga.fetchProfile)
-  ]);
+  const sagas = [
+    logIn.saga.submitLogIn,
+    news.saga.fetchNews,
+    profile.saga.fetchProfile
+  ];
+
+  yield all(
+    sagas.map(saga =>
+      spawn(function*() {
+        while (true) {
+          try {
+            yield call(saga);
+            break;
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      })
+    )
+  );
 }
